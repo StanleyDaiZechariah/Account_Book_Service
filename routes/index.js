e98c6789349d851b522930645f28c1052d8535a9
StanleyDaiZechariah <2606258311@qq.com>
2025-05-12 10:00:50 +0800

    添加了两个相应静态页面；实现获取静态资源实现页面样式效果

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// 记账本的列表
+router.get('/account', function (req, res, next) {
+  res.render('list');
+});
+
+router.get('/account/create', function (req, res, next) {
+  res.render('create');
 });
 
 module.exports = router;
